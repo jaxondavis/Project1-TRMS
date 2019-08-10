4733f323001b9ac2653c5b8ab2b6d08927aa3a74
DeontaK <44000428+DeontaK@users.noreply.github.com>
2019-08-10 11:54:47 -0400

    ""
    ""

---
+++ ReimbursementApp/src/main/webapp/js/login.js
@@ -0,0 +1,34 @@
+var submitLogin = () =>{
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = ()=>{
+        if(xhr.readyState == 4 && xhr.status == 200){
+            console.log(xhr.responseText);
+        }
+    }
+
+    xhr.open("GET", "http://localhost:8080/ReimbursementApp/login",true);
+    let payload = jsonBuilder();
+    xhr.send(payload);
+}
+
+var jsonBuilder = ()=>{
+    let form = document.querySelector("#loginForm");
+    //let email = form.querySelector("input[name = 'email']").value;
+    //let password = form.querySelector("input[name = 'password']").value;
+    let elements = form.elements;
+    let obj ={};
+   // obj.email = email;
+    //obj.password = password;
+    for(var i = 0 ; i < elements.length-1; i++){
+        var item = elements.item(i);
+        obj[item.name] = item.value;
+        console.log(obj);   
+    }
+    let json = JSON.stringify(obj);
+    return json;
+}
+
+window.onload = ()=>{
+   document.querySelector("#loginForm").addEventListener("submit", submitLogin,false); 
+}
